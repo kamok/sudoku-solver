bb6a81423a34e8ebb18e158c84f4d6661ca6f78b
Ka_Lenovo <heykamok@gmail.com>
2016-07-07 23:26:33 -0400

    Refactor the each cell run solve method.

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ Todo:
 
 1. Scan every cell. Pinpoint cells that only have 1 possible solution. Fill them in.
 2. Scan again. Repeat until solved? or no more possible.
+
+Repeat until no more freebies. 
+Then write out all possible board permutations.
+
+Run through a test for every single board configuration.
 3. Assuming branching appears where cells have 2+ possible answers, record branches.
 4. Run scan_for_freebee with that branch until solved? or cells have 2+ possible.
 5. Branch again.
--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -13,13 +13,12 @@ class Sudoku
   def solve
                           #wrap this inside @board.solved?
     until @board.solved?      #change to @board.no_more_freebies?
-      @board.cells.each do |cell|
-        cell.solve
-      end
+      @board.cells.each(&:solve)
       @board.update_possible_values
       @board.display_board
     end
                               #do the permutation brute force
+                              #check each permutation. is_permutation_valid?
 
     puts "Congrats. You're a cheater!"
   end
@@ -35,7 +34,7 @@ class Sudoku
 end
 
 a = Sudoku.new
-a.set_game(".....8..3.16.2.9.7.3...46...........9.5...2...2.13...9..3....2..7...5.........4..")
+a.set_game("39.82.7..8.15...69.2.16.4.3..2.96.58935...6.2.6.752.3.7.3941...2...3759..19...347")
 a.solve
 # a.board.cells.each do |cell| 
 #   p cell
